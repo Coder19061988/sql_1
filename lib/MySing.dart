3b03900ef740a31c06f65d7d33b32a8628f73872
AVese <aveselov88@gmail.com>
2022-08-25 22:56:52 +0300

    2022.08.25 22:55 ноут

--- lib/MySing.dart
+++ lib/MySing.dart
@@ -1,5 +1,16 @@
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
 class MySing{
 
-  static MySing
+static const _databasename = "DataBase.db";
+static const _version = 1;
+
+MySing._();
+
+//DataBase dataBase;
+
+
+
 
 }
